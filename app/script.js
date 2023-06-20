277751bb88c740269198680211e2833ca1f229aa
alafsasa <alafsasa@gmail.com>
2023-06-20 18:11:15 +0300

    second push

--- app/script.js
+++ app/script.js
@@ -1,11 +1,13 @@
 //Websocket variables
-const url = "ws://localhost:3000/myWebsocket"
+const url = "ws://localhost:3000?myCustomParam=1234&myCustomID=a1"
 const mywsServer = new WebSocket(url);
 
 //DOM Elements
 const myMessages = document.getElementById("messages");
 const myInput = document.getElementById("message");
 const sendBtn =document.getElementById("send");
+const userID = document.getElementById("uniqid");
+const sendTo = document.getElementById("toid");
 
 sendBtn.disabled = true;
 sendBtn.addEventListener("click", sendMsg, false);
@@ -13,8 +15,16 @@ sendBtn.addEventListener("click", sendMsg, false);
 //sending message from client
 function sendMsg(){
     const text = myInput.value;
+    const uid = userID.value;
+    const sendId = sendTo.value;
+    const sms = {
+        to: sendId,
+        from: uid,
+        messageBody: text
+    }
+    console.log(JSON.stringify(sms));
     msgGeneration(text, "Client");
-    mywsServer.send(text);
+    mywsServer.send(JSON.stringify(sms));
 }
 
 function msgGeneration(msg, from){
@@ -26,11 +36,25 @@ function msgGeneration(msg, from){
 //enabling send message when connection is open
 mywsServer.onopen = () => {
     sendBtn.disabled = false;
+    console.log('connection established');
 }
 
 //handling message event
 mywsServer.onmessage = (e) => {
     const { data } = e;
     msgGeneration(data, "Server");
+    //console.log('Message received', e, e.data);
 }
+
+//handle errors
+//mywsServer.onerror((e)=>{
+//    console.log('Websocket Error: ', e);
+//    //write a custom error handling func
+//    //errors also cause connections to close
+//});
+//
+////close connections
+//mywsServer.onclose((e)=>{
+//    console.log('Connection closed', e);
+//});
 
--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const WebSocket = require('ws');
 const app = express();
 const port = 3000;
 const path = require('path');
+const parse = require('url-parse');
+
+//users
+const users = {
+    1: 'a1',
+    2: 'b2',
+    3: 'c3'
+};
 
 app.use("/", express.static(path.resolve(__dirname, "app")));
 
@@ -20,16 +28,30 @@ const wsServer = new WebSocket.Server({
     noServer: true
 });
 
+//generate unique ids
+
+//var t = 0;
 //what should a websocket do on connection
-wsServer.on('connection', (ws)=>{
+wsServer.on('connection', (ws, req)=>{
+    //console.log(ws);
     //what to do on msg event
+    const params = parse(req.url, true);
+    console.log(params.query.myCustomID);
+    ws.id = params.query.myCustomID;
     ws.on('message', (msg)=>{
+        //console.log(JSON.parse(msg).to);
         wsServer.clients.forEach((client)=>{
             //check if client is ready
             if(client.readyState === WebSocket.OPEN){
-                client.send(msg.toString());
+                console.log('clientid: ' + client.id)
+                //console.log(client.id[0])
+                //client.send(msg.toString());
+                if(client.id === JSON.parse(msg).to){
+                    client.send(msg.toString());
+                }
             }
         });
+        //ws.send(msg.toString())
     });
 });
 
